f93c25ac9cf686b45afe4714b1be61f3a6eb4b9c
sanket0623 <sanketkhanna7@gmail.com>
2025-12-19 17:50:59 +0530

    static arribute and method

--- oops1.py
+++ oops1.py
@@ -1,9 +1,21 @@
 class Employee:
+
+    _uuid = 0 ## static attribute
+
     def __init__(self, emp_id, designation, salary):
-        self.id = emp_id
+        self.id = Employee._uuid
+        Employee._uuid += 1
         self.designation = designation
         self.salary = salary
         self.__name = "Default Name"
+
+    @staticmethod
+    def get_uuid():
+        return Employee._uuid
+
+    @staticmethod
+    def set_uuid(value):
+        Employee._uuid = value
 
     def get_name(self):
         return self.__name
@@ -13,6 +25,10 @@ class Employee:
 
 
 sam = Employee(123, 'Software Engineer', 20000)
-print(sam.get_name())
-sam.set_name('sanket')
-print(sam.get_name())
+print(Employee.get_uuid())
+
+Employee.set_uuid(100)
+print(Employee.get_uuid())
+
+sanket = Employee(123, 'Software Engineer', 20000)
+print(Employee.get_uuid())
